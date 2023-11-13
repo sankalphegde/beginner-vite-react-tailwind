6bc684312d614d776c6c0e01f11615938c78e284
sankalphegde <149039530+sankalphegde@users.noreply.github.com>
2023-11-13 16:06:33 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-500'>Hello Tailwindcss with react vite</h1>
+    </>
+  )
+}
+
+export default App
